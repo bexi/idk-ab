412393da467814a1b01dade53190cbadf0c66a34
bexi <trreitmaier@gmail.com>
2023-05-02 20:49:37 +0200

    feat: profile picture on the side when desktop

--- src/pages/about-karin.tsx
+++ src/pages/about-karin.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import {
   Anchor,
-  Card,
   Center,
   Container,
+  Group,
   Space,
   Text,
   Title,
+  MediaQuery,
 } from "@mantine/core";
 import colors from "../styles/colors";
 import { PageProps, Script } from "gatsby";
@@ -20,34 +21,42 @@ export default function Contact({ location }: PageProps) {
           Om I.D.K AB
         </Title>
         <Space h="md" />
-        <Text color={colors.black}>
-          I.D.K AB grundades 2022 och drivs av Karin Jannerborn som erbjuder
-          professionell samtalscoaching och förändringsledning till
-          privatpersoner och organisationer. I.D.K AB är verksamt i centrala
-          Göteborg men med hjälp av digitala möten kan du som klient befinna dig
-          precis var som helst. Karin började tidigt arbeta med människor och
-          har sedan examen från Personal och -arbetslivsprogrammet 2004 arbetat
-          i olika roller inom HR och ledarutveckling. ”För mig är det oerhört
-          meningsfullt och värdeskapande att få bidra till att utveckla
-          individer genom att både kunna stötta och utmana dem att hitta sin
-          inre drivkraft och det som är viktigt för dem.”
-        </Text>
+        <MediaQuery largerThan="sm" styles={{ flexWrap: "nowrap" }}>
+          <Group>
+            <Container px={0}>
+              <Text color={colors.black}>
+                I.D.K AB grundades 2022 och drivs av Karin Jannerborn som
+                erbjuder professionell samtalscoaching och förändringsledning
+                till privatpersoner och organisationer. I.D.K AB är verksamt i
+                centrala Göteborg men med hjälp av digitala möten kan du som
+                klient befinna dig precis var som helst. Karin började tidigt
+                arbeta med människor och har sedan examen från Personal och
+                -arbetslivsprogrammet 2004 arbetat i olika roller inom HR och
+                ledarutveckling. ”För mig är det oerhört meningsfullt och
+                värdeskapande att få bidra till att utveckla individer genom att
+                både kunna stötta och utmana dem att hitta sin inre drivkraft
+                och det som är viktigt för dem.”
+              </Text>
+              <Space h="md" />
+              <Text color={colors.black}>
+                "Mitt intresse för personlig utveckling i form av coaching ledde
+                mig 2020 till Gothia Akademi och tre terminer studier med fokus
+                på Katalyserande coaching och Förändringsledning. Jag är
+                Diplomerad Professionell Coach och Certifierad Förändringsledare
+                och arbetar enlig riktlinjerna satta av International Coach
+                Federation (ICF). Som bevis på min erfarenhet och kunskap
+                innehar jag sedan 2022 ICF certifiering (ACC)."
+              </Text>
+              <Space h="md" />
+            </Container>
+            <img
+              src="/karin_jannerborn_profil.jpg"
+              alt="karin"
+              style={{ maxWidth: "100%" }}
+            />
+          </Group>
+        </MediaQuery>
         <Space h="md" />
-        <Text color={colors.black}>
-          "Mitt intresse för personlig utveckling i form av coaching ledde mig
-          2020 till Gothia Akademi och tre terminer studier med fokus på
-          Katalyserande coaching och Förändringsledning. Jag är Diplomerad
-          Professionell Coach och Certifierad Förändringsledare och arbetar
-          enlig riktlinjerna satta av International Coach Federation (ICF). Som
-          bevis på min erfarenhet och kunskap innehar jag sedan 2022 ICF
-          certifiering (ACC)."
-        </Text>
-        <Space h="md" />
-        <img
-          src="/karin_jannerborn_profil.jpg"
-          alt="karin"
-          style={{ maxWidth: "100%" }}
-        />
         <Title order={2} color={colors.black}>
           Filosofi
         </Title>
